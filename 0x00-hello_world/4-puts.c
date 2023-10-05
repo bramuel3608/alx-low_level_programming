45a5b4592bc77422be7ef9a473f084e0e7db7d3b
bramuel3608 <ochiengbramuel@gmail.com>
2023-10-05 12:55:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /*
-*entry point
+* main-this is the entry point
+* Return: Always 0 when (succes)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
