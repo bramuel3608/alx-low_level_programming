793fe0845ee9c8560beab26e90a6a3ec2d72e3e0
bramuel3608 <ochiengbramuel@gmail.com>
2023-10-06 07:05:46 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-This is the entry point
+ * Return:Always 0
+ */
+
+int main(void)
+
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+
+}
