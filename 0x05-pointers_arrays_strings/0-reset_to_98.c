378cfa912c4f70ae29b7a299ef9afd3412f0de0f
bramuel3608 <ochiengbramuel@gmail.com>
2023-10-16 05:54:36 -0700

    Header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * @n - this is the value that should be updated to 98
+ * Return: Always 0
+ **/
+
+void reset_to_98(int *n);
+{
+
+*n = 98;
+
+}
