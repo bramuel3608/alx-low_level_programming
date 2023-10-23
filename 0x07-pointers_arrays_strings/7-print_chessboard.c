d4b8bd7823dec39dadabb5f56f149eda56ffcb70
bramuel3608 <ochiengbramuel@gmail.com>
2023-10-23 13:21:00 -0700

    this is a_putchar file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *print_chessboard - this is the entry point.
+ *@a: this is an array
+ *
+ *Return: always 0 (success)
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+}
