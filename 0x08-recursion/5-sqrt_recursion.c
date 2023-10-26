eb78af41d1c2ad793b497a6775456cb16ba36c60
bramuel3608 <ochiengbramuel@gmail.com>
2023-10-26 04:36:53 -0700

    function prints squar root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,21 +11,28 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion(n, 0));
+	else
+		return (actual_sqrt_recursion(n, 0));
 }
 
 /**
  *actual_sqrt_recursion- function returns square root of a number
- *@n: this is the number to find its square root
+ *@number: this is the number to find its square root
  *@i: this is an iterator
  *
  *Return: always 0
  */
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int number, int i)
 {
-	if (i * i > n)
+	if ((i * i) > number)
+	{
 		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	}
+	else if ((i * i) == number)
+	{
+		return (1);
+	}
+	else
+		return (actual_sqrt_recursion(number, i + 1));
+
 }
