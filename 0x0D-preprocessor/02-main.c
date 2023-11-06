180309edcf0a0f5e986ae7cdf15724da6f6e7953
bramuel3608 <ochiengbramuel@gmail.com>
2023-11-06 06:12:09 -0800

    prints file name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *main- funtion prints name of a file
+ *Return: 0 when success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
