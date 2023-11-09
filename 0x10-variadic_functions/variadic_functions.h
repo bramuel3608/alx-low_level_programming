cb593e18e79ea82d2fe872fc87347676f02739c8
bramuel3608 <ochiengbramuel@gmail.com>
2023-11-09 06:36:47 -0800

    This is the header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+Prototype: int sum_them_all(const unsigned int n, ...);
+Prototype: void print_numbers(const char *separator, const unsigned int n, ...);
+Prototype: void print_strings(const char *separator, const unsigned int n, ...);
+Prototype: void print_all(const char * const format, ...);
+
+#endif
