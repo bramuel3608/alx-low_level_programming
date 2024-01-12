1efc14e5c5f7565659b966042191e81e1721da66
bramuel3608 <ochiengbramuel@gmail.com>
2024-01-11 17:53:35 -0800

    Function frees memory in alist

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - function that free a dlistint list
+ * @head: type pointer dlistint node
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
+}
