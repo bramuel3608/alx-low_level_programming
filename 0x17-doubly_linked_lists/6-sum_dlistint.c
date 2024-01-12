5c2b98586e9f041c1a553348a109374a0f33c066
bramuel3608 <ochiengbramuel@gmail.com>
2024-01-11 18:11:12 -0800

    Function returns sum of data(n) of alist

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - return the nth node of a dlistint list
+ * @head: type pointer dlistint node
+ * Return: Return the sum of nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int add = 0;
+
+	while (head)
+	{
+		add = add + head->n;
+		head = head->next;
+	}
+	return (add);
+}
